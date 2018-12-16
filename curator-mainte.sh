41367b3414ef9f263c71f8c43892fc331db3e9e8
Hidetoshi Imai <hidepin@gmail.com>
2018-12-16 20:56:38 +0900

    Add sample setting.

--- curator-mainte.sh
+++ curator-mainte.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
-curator_cli --help
+CURATOR_INDEX_PATTERN="metricbeat|filebeat"
+CURATOR_FILTER=$(cat <<EOF
+                     {
+                        "filtertype": "pattern",
+                        "kind": "regex",
+                        "value": "^${CURATOR_INDEX_PATTERN}.*\$"
+                     },
+                     {
+                        "filtertype": "age",
+                         "source": "creation_date",
+                          "direction": "older",
+                           "unit": "days",
+                            "unit_count": 10
+                     }
+EOF
+)
+
+curator_cli show_indices --verbose --header --filter_list "[${CURATOR_FILTER}]"
